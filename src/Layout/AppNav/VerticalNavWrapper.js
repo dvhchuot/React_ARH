25b2fa2d936099d163e28e33884d2860921d8e7f
Hung Đăng Văn <dvhchuot@gmail.com>
2020-03-10 14:29:50 +0700

    init

---
+++ src/Layout/AppNav/VerticalNavWrapper.js
@@ -0,0 +1,47 @@
+import React, {Component, Fragment} from 'react';
+import {withRouter} from 'react-router-dom';
+
+import MetisMenu from 'react-metismenu';
+
+import RouterLink from 'react-metismenu-router-link';
+
+import {MainNav, ComponentsNav, FormsNav, WidgetsNav, ChartsNav} from './NavItems';
+
+class Nav extends Component {
+
+    state = {};
+
+    render() {
+        return (
+            <Fragment>
+                <h5 className="app-sidebar__heading">PRO VERSION</h5>
+                <div className="metismenu vertical-nav-menu">
+                    <ul className="metismenu-container">
+                        <li className="metismenu-item">
+                            <a className="metismenu-link" href="https://dashboardpack.com/theme-details/architectui-dashboard-react-pro" target="_blank">
+                                <i className="metismenu-icon pe-7s-diamond"></i>
+                                Upgrade to PRO
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+                <h5 className="app-sidebar__heading">Menu</h5>
+                <MetisMenu LinkComponent={RouterLink} content={MainNav} activeLinkFromLocation className="vertical-nav-menu" iconNamePrefix="" classNameStateIcon="pe-7s-angle-down"/>
+                <h5 className="app-sidebar__heading">UI Components</h5>
+                <MetisMenu LinkComponent={RouterLink} content={ComponentsNav} activeLinkFromLocation className="vertical-nav-menu" iconNamePrefix="" classNameStateIcon="pe-7s-angle-down"/>
+                <h5 className="app-sidebar__heading">Widgets</h5>
+                <MetisMenu LinkComponent={RouterLink} content={WidgetsNav} activeLinkFromLocation className="vertical-nav-menu" iconNamePrefix="" classNameStateIcon="pe-7s-angle-down"/>
+                <h5 className="app-sidebar__heading">Forms</h5>
+                <MetisMenu LinkComponent={RouterLink} content={FormsNav} activeLinkFromLocation className="vertical-nav-menu" iconNamePrefix="" classNameStateIcon="pe-7s-angle-down"/>
+                <h5 className="app-sidebar__heading">Charts</h5>
+                <MetisMenu LinkComponent={RouterLink} content={ChartsNav} activeLinkFromLocation className="vertical-nav-menu" iconNamePrefix="" classNameStateIcon="pe-7s-angle-down"/>
+            </Fragment>
+        );
+    }
+
+    isPathActive(path) {
+        return this.props.location.pathname.startsWith(path);
+    }
+}
+
+export default withRouter(Nav);
